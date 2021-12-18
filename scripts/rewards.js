253079d322daf8d3bd186ea8329b85f32016a7a1
Simone Rava <ravasimoneit@gmail.com>
2021-12-18 15:21:22 +0100

    add the script to rewards stakers

---
+++ scripts/rewards.js
@@ -0,0 +1,9 @@
+const TokenFarm = artifacts.require('TokenFarm')
+
+//questo script serve per far si che ogni volta che si chiama questa funzione si danno i rewardsçç
+module.exports = async function(callback){
+    let tokenFarm = await TokenFarm.deployed()
+    await tokenFarm.issueTokens()
+    console.log("rewards launched")
+    callback()
+}
